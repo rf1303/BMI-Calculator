567e64d7eb03b0cdc97f7bcd3d897fa3d4d46818
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-06-06 14:50:36 -0300

    detalles 01

--- assets/js/bmi-imperial.js
+++ assets/js/bmi-imperial.js
@@ -1,4 +1,4 @@
-
+import {resultPrint} from './bmi-metric.js';
 
 export function bmiImperial() {
      
@@ -8,9 +8,11 @@ export function bmiImperial() {
     const hin = +document.getElementById('height-in').value;
 
 
-    if (!hft === undefined || hft > 0 && !hin === undefined || hin > 0 && !wst === undefined || wst > 0 && !wlb === undefined || wlb > 0 ) {
-        const totalIn = ((hft * 12) + hin);
-        const totalLb = ((wst * 14) + wlb);
+    if (!hft === undefined || hft > 0 && !hin === undefined || hin > 0 && !wst === undefined || wst > 0 && !wlb === undefined || wlb > 0 ) { 
+
+        const totalIn = (hft * 12) + hin;
+        const totalLb = (wst * 14) + wlb;
+
         console.log('hft:',hft, ' hin: ', hin, ' wst: ', wst, ' wlb: ',wlb);
         console.log('totalIn: ', totalIn, ' totalLb: ', totalLb);
         calcImperial(totalLb, totalIn);
@@ -19,19 +21,20 @@ export function bmiImperial() {
     }
 }
 
-function calcImperial(totalIn, totalLb) {
-    const bmiResult = ((totalLb * 703) / (totalIn ** 2)).toFixed(1);
+export function calcImperial(totalLb, totalIn) {
+    const bmiResult = ((totalLb * 703) / (totalIn * totalIn)).toFixed(1);
+    console.log('bmiResult: ', bmiResult);
     const minW = ((18.5 * totalIn ** 2) / 703).toFixed(1);
     const maxW = ((24.9 * totalIn ** 2) / 703).toFixed(1);
     const resultMin = detailLb(minW);
     const resultMax = detailLb(maxW);
-    console.log('resultMin: ', resultMin, ' resultMax: ', resultMax);
-
+    console.log('resultMin: ', resultMin.stn, ' ',resultMin.lbs, ' resultMax: ', resultMax);
+    resultPrint(bmiResult, resultMin, resultMax);
 }
 
 function detailLb(imp) {
     const stn = Math.floor( imp / 14);
-    const lbs = stn % 14;
+    const lbs = Math.round(imp % 14);
     console.log('detailLb: ', stn, ' ',lbs);
     return {stn, lbs};
 }
--- assets/js/bmi-metric.js
+++ assets/js/bmi-metric.js
@@ -1,6 +1,4 @@
 
-
-
 export function bmiMetric() {
     const belowHeight = document.getElementById('height');
     const belowWeight = document.getElementById('weight');
@@ -9,33 +7,53 @@ export function bmiMetric() {
     console.log(belowHeight, belowWeight);
     if (!height === undefined || height > 0 && !weight === undefined || weight > 0) {
         console.log('belowHeight: ', height, ' belowWeight: ', weight);
-        calcMetric(height,weight); 
+        calcMetric(height, weight);
     } else {
         console.log('falta un campo');
     }
 }
 
-function calcMetric(height,weight) {
+function calcMetric(height, weight) {
     const heightM = height / 100;
     const bmiResult = (weight / (heightM * heightM)).toFixed();
     const resultMax = (24.9 * (heightM ** 2)).toFixed(1);
     const resultMin = (18.5 * (heightM ** 2)).toFixed(1);
-    console.log('bmiResult:',bmiResult,' resultMin: ',resultMin,' resultMax: ',resultMax);
+    console.log('bmiResult:', bmiResult, ' resultMin: ', resultMin, ' resultMax: ', resultMax);
 
     resultPrint(bmiResult, resultMin, resultMax)
 }
 
-function resultPrint(bmi, rMin, rMax) {
+export function resultPrint(bmi, rMin, rMax) {
+
+    const belowRadio = document.querySelector('.below__radio:checked');
     const belowWelcome = document.querySelector('.below__welcome');
     const resultHead = document.querySelector('.results__head');
     const resultScore = document.querySelector('.results__score');
     const resultDetail = document.querySelector('.results__details');
-    const metricDetail = document.querySelector('.details__bold--met');
-      
+    const detailBoldMet = document.querySelector('.details__bold--met');
+    const detailBoldImp = document.querySelector('.details__bold--imp');
+    const detailMetric = document.querySelector('.details__metric');
+    const detailImperial = document.querySelector('.details__imperial');
     belowWelcome.classList.add('display__none');
     resultHead.classList.remove('display__none');
     resultDetail.classList.remove('display__none');
     resultScore.textContent = `${bmi}`;
-    metricDetail.textContent = `${rMin}Kgs - ${rMax}Kgs.`;
+
+    switch (belowRadio.id) {
+        case 'metric':
+            console.log('metric:', rMin, ' ', rMax);
+            detailImperial.classList.add('display__none');
+            detailMetric.classList.remove('display__none');
+            detailBoldMet.textContent = `${rMin}Kgs - ${rMax}Kgs.`;
+            break;
+        case 'imperial':
+            console.log('imperial:', rMin.stn, ' ', rMin.lbs, ' ', rMax);
+            detailMetric.classList.add('display__none');
+            detailImperial.classList.remove('display__none');
+            detailBoldImp.textContent = `${rMin.stn}st ${rMin.lbs}lbs - ${rMax.stn}st ${rMax.lbs}lbs.`;
+        default:
+            console.log('ningun dato Radio');
+            break;
+    }
 }
 
